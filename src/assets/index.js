d8ff8bfbd84f3da1c3eadd36f9a449874e497a7b
Yusuf O. Yisa <iamyisas@gmail.com>
2026-08-02 12:09:13 +0100

    Add files via upload

---
+++ src/assets/index.js
@@ -0,0 +1,20 @@
+import art0 from './art (0).jpg';
+import art1 from './art (1).jpg';
+import art2 from './art (2).jpg';
+import art3 from './art (3).jpg';
+import art4 from './art (4).jpg';
+import art5 from './art (5).jpg';
+import art6 from './art (6).jpg';
+import art7 from './art (7).jpg';
+import art8 from './art (8).jpg';
+import art9 from './art (9).jpg';
+import art10 from './art (10).jpg';
+import art11 from './art (11).jpg';
+import art12 from './art (12).jpg';
+import art13 from './art (12).jpg';
+import art14 from './art (14).jpg';
+import video from './video.mp4';
+
+
+export const images = { art0, art1, art2, art3, art4, art5, art6, art7, art8, art9, art10, art11, art12, art13, art14, video };
+
